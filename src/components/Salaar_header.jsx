8b404aaf047fb63dbce6dab06f7be3caa4ee6d92
amardeeplakkuntla <lakkuntlaamardeep@gmail.com>
2026-01-04 18:52:47 +0530

    some changes

---
+++ src/components/Salaar_header.jsx
@@ -0,0 +1,11 @@
+import react from "react"
+
+export default function Salaar_header(){
+
+    return (
+
+    <>
+    <h1>SALAAR</h1>
+    </>
+    )
+}
---
+++ src/components/Salaar_profile.jsx
@@ -0,0 +1,14 @@
+import react from "react"
+
+export default function Salaar_profile(){
+
+    return(
+        <>
+       <h3>SALAAR IS A CELEBRATION </h3>
+       <h3>AN ADRENALINE RUSH GENARATED ON DEC 22 </h3>
+       <h3>A DINOSOUR BORN ON THAT DAY</h3>
+       <h3>AN EMOTION THAT HUANTS ME EVERYTIME</h3>
+       <h3>A EAGERNESS TO SEE PART 2</h3>
+        </>
+    )
+}
